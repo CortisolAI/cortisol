4973d86b6cc31528a29762c0c0515054fd89f98f
pm3310 <p.mitsoulis@gmail.com>
2023-07-30 12:14:06 +0100

    Add formatter black

--- cortisol/main.py
+++ cortisol/main.py
@@ -4,24 +4,24 @@ import typer
 
 from cortisol.commands.logs import app as log_commands
 
-_LOGO = '''
+_LOGO = """
    ____           _   _           _ 
   / ___|___  _ __| |_(_)___  ___ | |
  | |   / _ \| '__| __| / __|/ _ \| |
  | |__| (_) | |  | |_| \__ \ (_) | |
   \____\___/|_|   \__|_|___/\___/|_|
                                                                        
-'''
+"""
 
 app = typer.Typer()
 
+
 @app.callback()
 def display_logo():
-    """
-    """
     typer.echo(_LOGO)
 
-app.add_typer(log_commands, name='logs')
+
+app.add_typer(log_commands, name="logs")
 
 
 if __name__ == "__main__":
