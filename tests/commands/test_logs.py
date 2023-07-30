4cab4030986db129d4d0038613c53d4516850066
pm3310 <p.mitsoulis@gmail.com>
2023-07-30 12:14:06 +0100

    Add formatter black

--- tests/commands/test_logs.py
+++ tests/commands/test_logs.py
@@ -4,14 +4,31 @@ from cortisol.commands.logs import app
 
 runner = CliRunner()
 
+
 def test_cost_estimate_config_file_only_happy_case():
-    result = runner.invoke(app, ['--config', 'some_config_file.yaml'])
+    result = runner.invoke(app, ["--config", "some_config_file.yaml"])
 
     assert result.exit_code == 0
 
 
 def test_cost_estimate_configs_in_args_happy_case():
-    result = runner.invoke(app, ['-h', 'http://10.20.31.32:8000', '-l', '/path/to/logfile', '-u', '100', '-r', '30', '-t', '1h20m', '-f', 'some_cortisol_file.py'])
+    result = runner.invoke(
+        app,
+        [
+            "-h",
+            "http://10.20.31.32:8000",
+            "-l",
+            "/path/to/logfile",
+            "-u",
+            "100",
+            "-r",
+            "30",
+            "-t",
+            "1h20m",
+            "-f",
+            "some_cortisol_file.py",
+        ],
+    )
 
     assert result.exit_code == 0
 
@@ -23,6 +40,20 @@ def test_cost_estimate_no_args_given():
 
 
 def test_cost_estimate_one_missing_arg():
-    result = runner.invoke(app, ['-h', 'http://10.20.31.32:8000', '-l', '/path/to/logfile', '-u', '100', '-r', '30', '-t', '1h20m'])
+    result = runner.invoke(
+        app,
+        [
+            "-h",
+            "http://10.20.31.32:8000",
+            "-l",
+            "/path/to/logfile",
+            "-u",
+            "100",
+            "-r",
+            "30",
+            "-t",
+            "1h20m",
+        ],
+    )
 
     assert result.exit_code == 1
