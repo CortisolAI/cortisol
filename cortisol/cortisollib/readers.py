989e58eeb8765fa9b217f7972f7a7776a67e6119
Dionysis Varelas <dionvarelas@gmail.com>
2023-08-01 19:37:38 +0100

    added tests for readers

---
+++ cortisol/cortisollib/readers.py
@@ -0,0 +1,71 @@
+import os
+import docker
+
+
+def local_log_file_size_reader(file_path: str):
+    """
+    Reads log file size from a service running locally
+
+    :param file_path:
+    :type file_path:
+    :return:
+    :rtype:
+    """
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("File not found")
+
+        # Get the file size in bytes
+        file_size_bytes = os.path.getsize(file_path)
+        return file_size_bytes
+
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Error while accessing file: {e}")
+
+
+def docker_log_file_size_reader(container_id: str, file_path: str):
+    """
+    Reads log file size from a service running locally on docker
+
+    :param container_id:
+    :type container_id:
+    :param file_path:
+    :type file_path:
+    :return:
+    :rtype:
+    """
+    docker_client = docker.from_env()
+    try:
+        container = docker_client.containers.get(container_id)
+        exec_result = container.exec_run(["du", "-b", file_path], stdout=True, stderr=True, stream=True)
+
+        # Concatenate the stream content to get the output
+        exec_output = b"".join(exec_result.output)
+
+        # Parse the file size from the output (e.g., "123   /app/playground_app.log\n")
+        file_size_bytes = int(exec_output.split()[0])
+        return file_size_bytes
+
+    except docker.errors.NotFound:
+        raise Exception("Container not found")
+
+    except docker.errors.APIError as e:
+        raise Exception(f"Error while executing command in the container {e}")
+
+
+def log_file_size_reader(file_path: str, container_id: str = None):
+    """
+    Reads log file size from a service running locally with or without docker
+
+    :param file_path:
+    :type file_path:
+    :param container_id:
+    :type container_id:
+    :return:
+    :rtype:
+    """
+    if container_id != "":
+        print(f"Container ID: {container_id}")
+        return docker_log_file_size_reader(container_id, file_path)
+    else:
+        return local_log_file_size_reader(file_path)
---
+++ tests/cortisollib/test_readers.py
@@ -0,0 +1,81 @@
+from unittest import TestCase
+from collections import namedtuple
+import os
+import docker
+from unittest.mock import patch, MagicMock
+from cortisol.cortisollib.readers import (
+    docker_log_file_size_reader,
+    local_log_file_size_reader,
+    log_file_size_reader
+)
+
+ExecResult = namedtuple('ExecResult', 'exit_code,output')
+
+
+class TestFileSizeFunctions(TestCase):
+    @patch('docker.from_env')
+    def test_docker_log_file_size_reader_success(self, mock_docker):
+        # Mock the Docker client and container object
+        mock_container = mock_docker.return_value.containers.get.return_value
+        mock_stream = MagicMock(spec=docker.types.daemon.CancellableStream)
+        mock_stream.output = b'123   /app/playground_app.log\n'
+        mock_stream.__iter__.return_value = [b'123   /app/playground_app.log\n']
+
+        mock_container.exec_run.return_value = ExecResult(
+            exit_code=0,
+            output=mock_stream,  # Mock the output as CancellableStream
+        )
+
+        # Test file size reading from Docker container
+        container_id = 'test_container_id'
+        file_path = '/app/playground_app.log'
+        result = docker_log_file_size_reader(container_id, file_path)
+        self.assertEqual(result, 123)
+
+    def test_local_file_size_reader_success(self):
+        # Test file size reading from the local file system
+        file_path = 'test_file.txt'
+        with open(file_path, 'wb') as file:
+            file.write(b'This is a test file.')
+
+        result = local_log_file_size_reader(file_path)
+        self.assertEqual(result, 20)
+
+        # Clean up the test file
+        os.remove(file_path)
+
+    def test_local_file_size_reader_file_not_found(self):
+        # Test handling of FileNotFoundError for local file reader
+        file_path = 'non_existent_file.txt'
+        with self.assertRaises(FileNotFoundError):
+            local_log_file_size_reader(file_path)
+
+    @patch('docker.from_env')
+    def test_log_file_size_reader_with_container_id(self, mock_docker):
+        # Test get_file_size when container_id is provided
+        mock_container = mock_docker.return_value.containers.get.return_value
+        mock_stream = MagicMock(spec=docker.types.daemon.CancellableStream)
+        mock_stream.output = b'456   /app/playground_app.log\n'
+        mock_stream.__iter__.return_value = [b'456   /app/playground_app.log\n']
+
+        mock_container.exec_run.return_value = ExecResult(
+            exit_code=0,
+            output=mock_stream,  # Mock the output as CancellableStream
+        )
+
+        container_id = 'test_container_id'
+        file_path = '/app/playground_app.log'
+        result = log_file_size_reader(file_path, container_id)
+        self.assertEqual(result, 456)
+
+    def test_log_file_size_reader_without_container_id(self):
+        # Test get_file_size without container_id (use local_file_size_reader)
+        file_path = 'test_file.txt'
+        with open(file_path, 'wb') as file:
+            file.write(b'This is a test file.')
+
+        result = local_log_file_size_reader(file_path)
+        self.assertEqual(result, 20)
+
+        # Clean up the test file
+        os.remove(file_path)
